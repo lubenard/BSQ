eeae9ae2811c9bfe52710ffa81dd8df0891e95df
hjamet <lopilopilo24@gmail.com>
2018-08-20 11:05:36 +0200

    Updating ft_display 100%

--- src/ft_display.c
+++ src/ft_display.c
@@ -6,16 +6,28 @@
 /*   By: hjamet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 10:18:10 by hjamet            #+#    #+#             */
-/*   Updated: 2018/08/20 10:24:27 by hjamet           ###   ########.fr       */
+/*   Updated: 2018/08/20 11:05:28 by hjamet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_utils.h"
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 int		ft_display(char **tab, int x, int y)
 {
 	int x_var;
-	int y_var;
-	while ()
+
+	while (y >= 0)
+	{
+		x_var = 0;
+		while (x_var <= x)
+			ft_putchar(tab[y][x_var++]);
+		ft_putchar('\n');
+		y--;
+	}
 	return (0);
 }
