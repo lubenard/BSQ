de2df6a049c73c3e7f8b4b4c6246f335f4b8c40b
hjamet <lopilopilo24@gmail.com>
2018-08-21 16:35:43 +0200

    Optimization 78%

--- src/solve.c
+++ src/solve.c
@@ -6,7 +6,7 @@
 /*   By: hjamet <hjamet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 21:13:45 by hjamet            #+#    #+#             */
-/*   Updated: 2018/08/21 15:09:54 by hjamet           ###   ########.fr       */
+/*   Updated: 2018/08/21 16:35:05 by hjamet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ void		ft_optimize(int y1, int x1, int y2, int x2)
 	int		x;
 	int		y;
 
-	y = t.y;
-	if (t.tab[y2][x2] != c.vid)
-	{
-		x1 = x2;
-		y1 = y2;
-	}
+	y = y2;
 	while (y >= y1)
 	{
-		x = 0;
+		x = x2;
 		while (x <= x1)
-			t.tab[y][x++] = c.obs;
+		{
+			if (t.tab[y][x] != c.obs)
+				t.tab[y][x] = c.tem;
+			x++;
+		}
 		y--;
 	}
 }
@@ -54,26 +53,29 @@ int		ft_grow(int x, int y)
 	while (x + i + 1 <= t.x && y - i - 1 >= 0)
 	{
 		i++;
-		x_var = x;
-		y_var = y;
-		while (x_var <= (x + i))
+		x_var = x + i;
+		y_var = y - i;
+		while (x_var >= x)
 		{
 			if (t.tab[y - i][x_var] != c.vid || t.tab[y_var][x + i] != c.vid)
 			{
 				ft_sav(i, x, y);
-				//ft_optimize(y - i, x_var, y_var, x + i);
+				if (t.tab[y - i][x_var] != c.vid)
+					ft_optimize(y - i, x_var, y, x);
+				else
+					ft_optimize(y_var, x + i, y, x);
 				ft_display(t);
 				ft_putchar('\n');
 				return (0);
 			}
-			x_var++;
-			y_var--;
+			x_var--;
+			y_var++;
 		}
 	}
 	i++;
 	ft_sav(i, x, y);
-	ft_display(t);
-	ft_putchar('\n');
+//	ft_display(t);
+//	ft_putchar('\n');
 	return (0);
 }
 
